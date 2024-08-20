14289da8c5dd40d12c38775655b83fa5cd18dd9c
NeilRow56 <neilrowland56@gmail.com>
2024-08-20 04:52:00 +0100

    Kinde authentication setup with Two Factor authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
+
   return (
-    <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Home Page</h1>
+    <div className=" p-10 space-x-8">
+      {session ? (
+        <div>
+          <LogoutLink>
+            <Button>Logout</Button>
+          </LogoutLink>
+        </div>
+      ) : (
+        <div className="space-x-4">
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+          <RegisterLink>
+            <Button>Register</Button>
+          </RegisterLink>
+        </div>
+      )}
     </div>
   );
 }
